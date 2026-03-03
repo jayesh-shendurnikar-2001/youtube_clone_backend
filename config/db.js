4f3c542c7c86cba297479a2d6788feee0f5aa5fe
jayeshshendurnikar <shendurnikarjayesh2001@gmail.com>
2026-03-03 10:08:50 +0530

    DOTENV added

--- config/db.js
+++ config/db.js
@@ -4,12 +4,11 @@ import mongoose from "mongoose";
 async function connectDb(){
 
     try {
-        await mongoose.connect("mongodb+srv://jayesh:jayesh@cluster0.oo1gdox.mongodb.net/youtube")
+        await mongoose.connect(process.env.MONGO_URI)
         console.log("MongoDb is connected")
     } catch (error) {
         console.error(`MongoDb connection error ${error}`)
     }
-
 }
 
 export default connectDb
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 import express from "express"
 import connectDb from "./config/db.js"
+import dotenv from "dotenv"
 
 const app = express()
 
 app.use(express.json())
-
+dotenv.config();
 connectDb()
 
-app.listen(3000 , () => {
-    console.log("app is running on port 3000")
+app.listen(process.env.PORT , () => {
+    console.log(`app is running on port ${process.env.PORT}`)
 })
