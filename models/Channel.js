e7fe4d15d7b1262de74e68266da4ff03beaecdbf
jayeshshendurnikar <shendurnikarjayesh2001@gmail.com>
2026-03-03 10:13:46 +0530

    Added channel model schema

---
+++ models/Channel.js
@@ -0,0 +1,40 @@
+// models/Channel.js – Channel model
+import mongoose from "mongoose";
+
+const channelSchema = new mongoose.Schema(
+    {
+        channelName: {
+            type: String,
+            required: [true, "Channel name is required"],
+            unique: true,
+            trim: true,
+        },
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        description: {
+            type: String,
+            default: "",
+        },
+        channelBanner: {
+            type: String,
+            default: "",
+        },
+        subscribers: {
+            type: Number,
+            default: 0,
+        },
+        videos: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Video",
+            },
+        ],
+    },
+    { timestamps: true }
+);
+
+const Channel = mongoose.model("Channel", channelSchema);
+export default Channel;
