8dc511cfa25a52c78eebd29581d8288f177efbfe
jayeshshendurnikar <shendurnikarjayesh2001@gmail.com>
2026-03-03 10:54:36 +0530

    added login user login

--- controller/authController.js
+++ controller/authController.js
@@ -50,3 +50,43 @@ export const registerUser = async (req , res) => {
         res.status(500).json({ message: error.message });
       }
 }
+
+export const loginUser = async (req, res) => {
+    try {
+      const { email, password } = req.body;
+  
+      if (!email || !password) {
+        return res
+          .status(400)
+          .json({ message: "Please provide email and password" });
+      }
+  
+      const user = await User.findOne({ email });
+  
+      if (!user) {
+        return res.status(401).json({ message: "Invalid email or password" });
+      }
+  
+      const isMatch = await bcrypt.compare(password, user.password);
+  
+      if (!isMatch) {
+        return res.status(401).json({ message: "Invalid email or password" });
+      }
+  
+      const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "7d",
+      });
+  
+      res.status(200).json({
+        message: "Login successful",
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+        token,
+      });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  };
+
+
